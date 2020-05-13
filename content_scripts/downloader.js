02cc526ef3a8c6285388ca7e00e4818b4629fced
Anton Kahwaji <anton.kahwaji@gmail.com>
2020-05-13 03:02:52 +0200

    Downloading raw files progress.

--- content_scripts/downloader.js
+++ content_scripts/downloader.js
@@ -35,6 +35,28 @@
 		return urlsToDownload;
 	}
 
+	function createProgressBar(file) {
+		let downloadStatusDivElement = document.getElementById('cbd-download-status');
+
+		let progressBarParentDivElement = document.createElement('div');
+		progressBarParentDivElement.id = `cbd-progress-bar-${file}`;
+		progressBarParentDivElement.classList.add("cbd-progress");
+
+		let progressBarElement = document.createElement('div');
+		progressBarElement.classList.add("cbd-progress-bar");
+
+		let progressTextElement = document.createElement('div');
+		progressTextElement.classList.add("cbd-progress-text");
+
+		progressBarParentDivElement.appendChild(progressBarElement);
+		progressBarParentDivElement.appendChild(progressTextElement);
+
+
+		let resultStatusElement = document.getElementById('cbd-result');
+
+		downloadStatusDivElement.insertBefore(progressBarParentDivElement, resultStatusElement);
+	}
+
 	function prepareDownloadProgress() {
 		let downloadStatus = document.querySelector('body > #cbd-download-status');
 
@@ -47,22 +69,11 @@
 		let downloadStatusDivElement = document.createElement('div');
 		downloadStatusDivElement.id = "cbd-download-status";
 
-		let progressBarParentDivElement = document.createElement('div');
-		progressBarParentDivElement.id = "cbd-progress-bar";
-		progressBarParentDivElement.classList.add("cbd-progress");
-		progressBarParentDivElement.classList.add("cbd-hide");
-
-		let progressBarElement = document.createElement('div');
-		progressBarElement.classList.add("cbd-progress-bar");
-
 		let resultStatusElement = document.createElement('p');
 		resultStatusElement.id = "cbd-result";
 		resultStatusElement.textContent = "Download process will start soon!";
 
 
-		progressBarParentDivElement.appendChild(progressBarElement);
-
-		downloadStatusDivElement.appendChild(progressBarParentDivElement);
 		downloadStatusDivElement.appendChild(resultStatusElement);
 
 
@@ -82,6 +93,7 @@
 		let resultElement = document.getElementById("cbd-result");
 		resultElement.classList.add("cbd-alert", "cbd-alert-success");
 		resultElement.textContent = text;
+		resultElement.title = "If it seems slow or that it stopped, please be patient, trust me, it's working. It didn't stop, neither did it break, it just doesn't send enough notifications so this message can be updated correctly.";
 	}
 
 	function showFinalMessage(text) {
@@ -108,24 +120,52 @@
 		resultElement.textContent = text;
 	}
 
-	function updatePercent(percent) {
-		let progressBarParentElement = document.getElementById("cbd-progress-bar");
-		progressBarParentElement.classList.remove("cbd-hide");
+	function updatePercent(percent, file) {
+		let progressBarParentElement = document.getElementById(`cbd-progress-bar-${file}`);
 		let progressBarElement = progressBarParentElement.getElementsByClassName("cbd-progress-bar")[0];
-		// progressBarElement.setAttribute("aria-valuenow", percent);
+		let progressTextElement = progressBarParentElement.getElementsByClassName("cbd-progress-text")[0];
 		progressBarElement.style.width = percent + "%";
+		progressTextElement.textContent = `${percent.toFixed(2)}% ${file}`;
+
+		if (percent >= 100) {
+			let linkElement = document.createElement('a');
+			linkElement.id = `cbd-clear-progress-${file}`;
+			linkElement.textContent = " click here to close progress bar.";
+			linkElement.setAttribute("style", "color: red;");
+
+			linkElement.addEventListener('click', () => {
+				progressBarParentElement.parentNode.removeChild(progressBarParentElement);
+			});
+
+			progressTextElement.appendChild(linkElement);
+		}
 	}
 
-	function urlToPromise(url) {
+	function urlToPromise(url, progress_callback) {
 		return new Promise(function (resolve, reject) {
-			JSZipUtils.getBinaryContent(url, function (err, data) {
-				if (err) {
-					reject(err);
-				} else {
-					resolve(data);
-				}
+			JSZipUtils.getBinaryContent(url, {
+				callback: function (err, data) {
+					if (err) {
+						reject(err);
+					} else {
+						resolve(data);
+					}
+				},
+				progress: progress_callback
 			});
 		});
+	}
+
+	function progressClosure(file) {
+		const progressDownload = file => metadata => {
+			let percent = metadata.percent;
+			if (isNaN(percent)) {
+				percent = 0;
+			}
+			updatePercent(percent, file);
+		};
+
+		return progressDownload(file);
 	}
 
 	function urlTextContent(url) {
@@ -142,18 +182,24 @@
 		zip.file('create_lecture.ps1', urlTextContent(downloadSelection.scriptUrl), { binary: true })
 
 		if (downloadQueue.video) {
-			zip.file('video.webm', urlToPromise(downloadQueue.video), { binary: true });
+			createProgressBar("video");
+			const progress_callback = progressClosure("video");
+			zip.file('video.webm', urlToPromise(downloadQueue.video, progress_callback), { binary: true });
 		}
 
 		if (downloadQueue.audio) {
-			zip.file('audio.webm', urlToPromise(downloadQueue.audio), { binary: true });
+			createProgressBar("audio");
+			const progress_callback = progressClosure("audio");
+			zip.file('audio.webm', urlToPromise(downloadQueue.audio, progress_callback), { binary: true });
 		}
 
 		if (downloadQueue.slides) {
 			let slidesFolder = zip.folder('slides');
 			let timesFileContent = [];
 			for (const [index, slide] of downloadQueue.slides.entries()) {
-				slidesFolder.file(`slide-${index}.png`, urlToPromise(slide.url), { binary: true });
+				createProgressBar(`slide-${index}`);
+				const progress_callback = progressClosure(`slide-${index}`);
+				slidesFolder.file(`slide-${index}.png`, urlToPromise(slide.url, progress_callback), { binary: true });
 				timesFileContent.push(`${slide.start}-${slide.end}`);
 			}
 			timesFileContent = timesFileContent.join('\n');
@@ -166,7 +212,6 @@
 				msg += ", current file = " + metadata.currentFile;
 			}
 			showMessage(msg);
-			updatePercent(metadata.percent | 0);
 		})
 			.then(blob => {
 				let lectureTitle = document.getElementById("recording-title").textContent;
